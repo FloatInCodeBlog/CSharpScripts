8eded930adbf7bc3323d0dcb95d26af5489ab9b9
Floatas <floatas@outlook.com>
2015-07-19 23:06:31 +0300

    Initial commit

---
+++ CSharpScripts/Program.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.Scripting.CSharp;
+using System;
+
+namespace CSharpScripts
+{
+    public class Program
+    {
+        public class GlobalParams
+        {
+            public int X;
+            public int Y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("EvalWithoutParameters");
+            EvalWithoutParameters();
+
+            Console.WriteLine("EvalExpression");
+            EvalExpression();
+
+            Console.WriteLine("ScriptCreateAndRun");
+            ScriptCreateAndRun();
+
+            Console.WriteLine("ScriptWithNamespace");
+            ScriptWithNamespace();
+
+            Console.ReadLine();
+        }
+
+        public static void EvalWithoutParameters()
+        {
+            var result = CSharpScript.Eval(@"25 + 30", ScriptOptions.Default);
+            Console.WriteLine("Eval: " + result);
+        }
+
+        public static void EvalExpression()
+        {
+            var result = CSharpScript.Eval(@"X + Y", ScriptOptions.Default, new GlobalParams { X = 12, Y = 25 });
+            Console.WriteLine("Eval: " + result);
+        }
+
+        public static void ScriptCreateAndRun()
+        {
+            var script = CSharpScript.Create(@"var result = X + Y;", ScriptOptions.Default);
+            var state = script.Run(new GlobalParams { X = 10, Y = 2 });
+            var state2 = script.Run(new GlobalParams { X = 1, Y = 3 });
+            Console.WriteLine("State variable: " + state.Variables["result"].Value);
+            Console.WriteLine("State2 variable: " + state2.Variables["result"].Value);
+        }
+
+        public static void ScriptWithNamespace()
+        {
+            var scriptOptions = ScriptOptions.Default.AddNamespaces("System.IO");
+            var script = CSharpScript.Create(@"var result = Path.Combine(""folder"", ""file"");", scriptOptions);
+            var state = script.Run();
+            Console.WriteLine("State variable: " + state.Variables["result"].Value);
+        }
+    }
+}
